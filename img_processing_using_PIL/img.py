2b77207bde00621a9a75adf19068ad057ca90eb8
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2023-07-27 22:54:14 +0530

    Update and rename img.py to img_processing_using_PIL/img.py

--- img_processing_using_PIL/img.py
+++ img_processing_using_PIL/img.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageFilter
 
-img = Image.open(r'C:\Users\vedant\OneDrive\Desktop\New folder (5)\Image_processing\images\Neon-Tetra-4.png')
+img = Image.open(r'.\images\Neon-Tetra-4.png')
 
 # Convert the image to RGB mode
 img_rgb = img.convert("RGB")
@@ -10,7 +10,7 @@ new = img_rgb.filter(ImageFilter.SMOOTH_MORE)
 
 # Save the filtered image
 name = input("Enter name of png file you want to create: ")
-save_path = f"C:\\Users\\vedant\\OneDrive\\Desktop\\New folder (5)\\Image_processing\\images\\{name}.png"
+save_path = f".\\images\\{name}.png"
 print(f"Saving the filtered image to: {save_path}")
 new.save(save_path, 'PNG')
 
