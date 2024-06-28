475efa262acbb616d30a69ee1cbfbf47a4d6d98a
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2024-06-28 16:09:16 +0530

    Add files via upload

---
+++ Selenium.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+ 
+chrome_browser = webdriver.Chrome()
+chrome_browser.maximize_window()
+chrome_browser.get('https://demo.seleniumeasy.com/basic-first-form-demo.html')
+print("Selenium Easy Demo"in chrome_browser.title) # To Check it the tile is present 
+
+# This solves the issue with the Popup for those that encounter it:
+chrome_browser.implicitly_wait(2)
+popup =chrome_browser.find_element(By.ID, 'at-cv-lightbox-close')
+popup.click()
+ 
+ 
+ 
+user_message = chrome_browser.find_element(By.ID, 'user-message')
+user_message.clear()
+user_message.send_keys('I AM EXTRA COOOOL')
+ 
+time.sleep(2)
+show_message_button = chrome_browser.find_element(By.CLASS_NAME, 'btn-default')
+show_message_button.click()
+ 
+output_message = chrome_browser.find_element(By.ID, 'display')
+assert 'I AM EXTRA COOOOL' in output_message.text
